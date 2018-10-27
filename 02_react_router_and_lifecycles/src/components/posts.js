02316833c2c531e433e352c4d77aeaa9fad3fecf
rinatsafin <safinrinat87@gmail.com>
2018-10-28 01:03:41 +0300

    render arrays lesson 34

--- 02_react_router_and_lifecycles/src/components/posts.js
+++ 02_react_router_and_lifecycles/src/components/posts.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Posts = () => {
-  return (
-    <div>
-      <Link to="/posts/1/fransis">post fransis</Link>
-      <br />
-      <Link to="/posts/2/mary">post mary</Link>
-      <br />
-      <Link to="/posts/3/jhon">post jhon</Link>
-    </div>
-  );
+  // const listId = [
+  //   { id: 1, name: "Post 1", userName: "Fransis" },
+  //   { id: 2, name: "Post 2", userName: "Joe" },
+  //   { id: 3, name: "Post 3", userName: "Jhon" }
+  // ];
+  return [
+    <div key="1">Hello</div>,
+    <div key="2">i am</div>,
+    <div key="3">a react app</div>
+  ];
+  // return <div>
+  //   {/* one way: */}
+  //     {/* {listId.map(item => <div>
+  //         <Link key={item.id} to={`posts/${item.id}/${item.userName}`}>
+  //           {item.name}
+  //         </Link>
+  //       </div>)} */}
+  //   </div>;
 };
 
 export default Posts;
