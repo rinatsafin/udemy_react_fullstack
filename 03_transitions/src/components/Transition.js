b396df49f8bec340306c68e75e50663a33cca289
rinatsafin <safinrinat87@gmail.com>
2018-10-28 16:38:52 +0200

    Toggle with transition lesson 39

--- 03_transitions/src/components/Transition.js
+++ 03_transitions/src/components/Transition.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
-import '../css/App.css';
+import React, { Component } from "react";
+import Transition from "react-transition-group/Transition";
+import "../css/App.css";
 
-class TransitionComp extends Component{
+class TransitionComp extends Component {
+  state = {
+    show: true
+  };
 
-    render(){
-        return(
-            <div>
-                Transition
+  timing = 2000;
+
+  toggleDiv = () => {
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
+
+  render() {
+    const { show } = this.state;
+    return (
+      <div>
+        <Transition in={show} timeout={this.timing} mountOnEnter unmountOnExit>
+          {state => (
+            <div
+              style={{
+                backgroundColor: "#ec1561",
+                height: "100px",
+                transition: `all ${this.timing / 1000}s ease`,
+                padding: "20px",
+                margin: "10px",
+                color: "#fff",
+                fontSize: "30px",
+                opacity: state === "entered" || state === "exiting" ? 0 : 1
+              }}
+            >
+              {state}
             </div>
-
-        )
-    }
+          )}
+        </Transition>
+        <div onClick={this.toggleDiv} className="showDiv">
+          SHOW / HIDE
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default TransitionComp;
